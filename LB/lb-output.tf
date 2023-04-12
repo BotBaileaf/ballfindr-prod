18152a23720a2fce842f9064299018283c459200
BotBaileaf <bailey.brucculeri@outlook.com>
2023-04-12 02:19:36 -0400

    overhaul test

--- LB/lb-output.tf
+++ LB/lb-output.tf
@@ -1,7 +1,7 @@
 output "asg-tg" {
     value = aws_lb_target_group.asg-tg
 }
-output "alb_dns_name" {
-    value = aws_lb.loadbalancer.dns_name
-    description = "DNS name of LB"
-}
+# output "alb_dns_name" {
+#     value = aws_lb.loadbalancer.dns_name
+#     description = "DNS name of LB"
+# }
