32657cff4dbdd11bd16314cd830a8d8d8d8f173c
BotBaileaf <bailey.brucculeri@outlook.com>
2023-04-12 02:13:38 -0400

    overhaul test

--- EC2/create-ec2.tf
+++ EC2/create-ec2.tf
@@ -1,27 +1,51 @@
-data "aws_ami" "amazon_linux" {
-	most_recent = true
-	owners = ["amazon"]
-	filter {
-		name = "name"
-		values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+# data "aws_ami" "amazon_linux" {
+# 	most_recent = true
+# 	owners = ["amazon"]
+# 	filter {
+# 		name = "name"
+# 		values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+# 	}
+# }
+
+# resource "aws_instance" "ec2-public" {
+# 	ami = data.aws_ami.amazon_linux.id
+# 	instance_type = var.chassis
+# 	key_name = "LAB2-KEY"
+# 	subnet_id = var.aws_subnet-public-SN
+# 	security_groups = [var.SEC-GR-TF]
+# 	tags = { Name = "EC2-TF-Public" }
+# 	user_data = file("user-data.sh")
+# }
+# resource "aws_instance" "ec2-private" {
+# 	ami = data.aws_ami.amazon_linux.id
+# 	instance_type = var.chassis
+# 	key_name = "LAB2-KEY"
+# 	subnet_id = var.aws_subnet-private-SN
+# 	security_groups = [var.SEC-GR-TF]
+# 	tags = { Name = "EC2-TF-Private" }
+# 	user_data = file("user-data.sh")
+# }
+
+resource "aws_launch_configuration" "ec2cluster" {
+	image_id = "ami-0ed9277fb7eb570c9"
+	instance_type = "t2.micro"
+ 	security_groups = [var.SEC-GR-TF]
+	user_data = file("user-data.sh")
+	lifecycle {
+	  create_before_destroy = true
 	}
 }
 
-resource "aws_instance" "ec2-public" {
-	ami = data.aws_ami.amazon_linux.id
-	instance_type = var.chassis
-	key_name = "LAB2-KEY"
-	subnet_id = var.aws_subnet-public-SN
-	security_groups = [var.SEC-GR-TF]
-	tags = { Name = "EC2-TF-Public" }
-	user_data = file("user-data.sh")
-}
-resource "aws_instance" "ec2-private" {
-	ami = data.aws_ami.amazon_linux.id
-	instance_type = var.chassis
-	key_name = "LAB2-KEY"
-	subnet_id = var.aws_subnet-private-SN
-	security_groups = [var.SEC-GR-TF]
-	tags = { Name = "EC2-TF-Private" }
-	user_data = file("user-data.sh")
+resource "aws_autoscaling_group" "asg" {
+	launch_configuration = aws_launch_configuration.ec2cluster.name
+	vpc_zone_identifier = var.aws_subnet-public-SN
+	target_group_arns = var.asg-tg
+	health_check_type = "ELB"
+	min_size = 2
+	max_size = 4
+	tag {
+	  key = "Name"
+	  value = "asg"
+	  propagate_at_launch = true
+	}
 }
--- LB/create-lb.tf
+++ LB/create-lb.tf
@@ -1,47 +1,54 @@
 resource "aws_lb" "TF_LB" {
   name               = "TF_LB"
-  internal           = false
   load_balancer_type = "application"
   security_groups = [var.SEC-GR-TF]
-
-  subnet_mapping {
-    subnet_id = var.aws_subnet-public-SN
-    allocation_id = aws_eip.example_eip.id
-  }
-
+  subnets = var.aws_subnet-public-SN
   tags = {
     Name = "TF_LB"
   }
 }
 
-resource "aws_lb_target_group" "example_tg" {
-  name_prefix = "example_tg_"
-  port = 80
-  protocol = "HTTP"
-  vpc_id      = var.vpc-id
-  target_type = "instance"
-  health_check {
-    healthy_threshold = 3
-    unhealthy_threshold = 3
-    interval = 30
-    timeout = 5
-    path = "/"
-    port = "traffic-port"
-  }
-}
-
-resource "aws_lb_target_group_attachment" "example_tg_attachment" {
-  target_group_arn = aws_lb_target_group.example_tg.arn
-  target_id        = var.ec2_instance
-  port             = 80
-}
-
-resource "aws_lb_listener" "example_listener" {
-  load_balancer_arn = aws_lb.example_lb.arn
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.TF_LB.arn
   port = "80"
   protocol = "HTTP"
   default_action {
-    type = "forward"
-    target_group_arn = aws_lb_target_group.example_tg.arn
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: Page not found :("
+      status_code = 404
+    }
   }
 }
+
+resource "aws_lb_target_group" "asg-tg" {
+  name_prefix = "asg-tg"
+  port = 80
+  protocol = "HTTP"
+  vpc_id      = var.vpc-id
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    interval = 30
+    timeout = 5
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+  }
+}
+
+resource "aws_lb_listener_rule" "asg-listen" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.asg-tg.arn
+  }
+}
+
