ea4c99e646ade0c4d9d74dfbbeea20aaf04e3776
Bailey Brucculeri <baileybrucculeri@Baileys-MacBook-Pro.local>
2023-04-11 22:12:09 -0400

    more fixes

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,18 @@
-  cloud {
-    organization = "ballfindr"
-    workspaces {
-      name = "ballfindr-prod"
+terraform {
+    cloud {
+      organization = "ballfindr"
+
+      workspaces {
+        name = "ballfindr-prod"
+      }
     }
+
+    required_providers {
+      aws = {
+        source  = "hashicorp/aws"
+        version = "~> 3.28.0"
+      }
+    }
+
+    required_version = ">= 0.14.0"
   }
