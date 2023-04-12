6e589b116a76bca87f23d280739602387c382e98
Bailey Brucculeri <baileybrucculeri@Baileys-MacBook-Pro.local>
2023-04-11 22:13:23 -0400

    more fixes

--- terraform.tf
+++ terraform.tf
@@ -6,13 +6,5 @@ terraform {
         name = "ballfindr-prod"
       }
     }
-
-    required_providers {
-      aws = {
-        source  = "hashicorp/aws"
-        version = "~> 3.28.0"
-      }
-    }
-
     required_version = ">= 0.14.0"
   }
