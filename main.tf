4c385ed69b17b256326ea88a3acfc0c7d4e01adf
BotBaileaf <bailey.brucculeri@outlook.com>
2023-04-11 22:33:25 -0400

    idk

--- main.tf
+++ main.tf
@@ -1,15 +1,22 @@
-  terraform {
-      cloud {
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  cloud {
     organization = "ballfindr"
+
     workspaces {
       name = "ballfindr-prod"
     }
-  }
-  required_providers {
-   aws = {
-     source = "hashicorp/aws"
-     version = "4.57.1"
-   }
   }
 }
 
