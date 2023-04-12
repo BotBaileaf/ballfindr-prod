f2f3a4d7322b209083b2e9ba05b7b5cfc23057f8
BotBaileaf <bailey.brucculeri@outlook.com>
2023-04-12 02:22:33 -0400

    overhaul test

--- EC2/create-ec2.tf
+++ EC2/create-ec2.tf
@@ -38,8 +38,8 @@ resource "aws_launch_configuration" "ec2cluster" {
 
 resource "aws_autoscaling_group" "asg" {
 	launch_configuration = aws_launch_configuration.ec2cluster.name
-	vpc_zone_identifier = var.aws_subnet-public-SN
-	target_group_arns = var.asg-tg
+	vpc_zone_identifier = var.aws_subnet-public-SN.ids
+	target_group_arns = var.asg-tg.arn
 	health_check_type = "ELB"
 	min_size = 2
 	max_size = 4
